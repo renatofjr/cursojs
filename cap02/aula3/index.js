b2a604bad9844cdf2114d2ede5310d0763d486dd
renatofjr <renatofjr@gmail.com>
2022-11-18 09:27:17 -0300

    'update'

---
+++ cap02/aula3/index.js
@@ -0,0 +1,5 @@
+console.log('renato');
+console.log("renato");
+console.log(`'renato "freire"`);
+
+console.log(35, 15.85, 'renato');
---
+++ cap02/aula4/comentarios.js
@@ -0,0 +1,14 @@
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+
+/* 
+Um bloco de código
+Linha de comentário
+*/ 
+
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
