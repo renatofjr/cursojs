946695262102711bc735b7d143e201dd7e86c403
renatofjr <renatofjr@gmail.com>
2022-11-18 15:28:31 -0300

    update

---
+++ cap02/aula6/index.js
@@ -0,0 +1,26 @@
+let nome = 'João'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+let nome1; // Declarou a variável
+nome1 = 'Qualquer valor' // Iniciailizando a variável
+console.log(nome1);
+nome1 = 'Renato';
+console.log(nome1);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisa ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase - nomeCompleto
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET
+let nomeCliente = 'Luiz';
+nomeCliente = 'Otávio';
+
+console.log(nomeCliente);
---
+++ cap02/aula7/index.js
@@ -0,0 +1,16 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisa ter nomes significativos
+// Não pode começar o nome de uma contante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase - nomeCompleto
+// Case-sensitive
+// Não podemos redeclarar constantes com let
+// NÃO UTILIZE VAR, UTILIZE LET
+
+const nome = 'João';
+console.log(nome);
+
+const primeiroNum = 5;
+const segundoNum = 10;
+const result = primeiroNum * segundoNum;
+const resultDuplicado = result * 2;
